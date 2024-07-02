4540d7a6c980f120fa4f34ba802a3a3eaac2b804
segun <segundo.marcaida@ing.austral.edu.ar>
2024-07-02 02:06:06 +0200

    initial commit

--- src/amplitude.js
+++ src/amplitude.js
@@ -1,6 +1,6 @@
 import * as amplitude from '@amplitude/analytics-browser';
 
-const AMPLITUDE_API_KEY = '1426d3a0ad525b278a597824506af2c6'; // Replace with your Amplitude API key
+const AMPLITUDE_API_KEY = process.env.REACT_APP_AMPLITUDE_API_KEY;
 
 export const initializeAmplitude = () => {
     amplitude.init(AMPLITUDE_API_KEY);
